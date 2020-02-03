94ae237a2e000dbf46f18b68a892b2687759ce55
Kyle Bustard <bustardkyle@gmail.com>
2020-02-02 19:35:54 -0600

    Implement `Deck.shuffle` method

--- __tests__/deck.test.js
+++ __tests__/deck.test.js
@@ -4,23 +4,58 @@ describe('Given a deck of playing cards', () => {
   const deck = new Deck();
   deck.create();
 
+  function _propSetInDeck(cards, prop, propSize) {
+    const propSet = new Set();
+    let i = 0;
+    while (propSet.size < propSize) {
+      if (!propSet.has(cards[i][prop])) {
+        propSet.add(cards[i][prop]);
+      }
+      i++;
+    }
+    return propSet;
+  }
+
   it('should have the 4 suits', () => {
     const expectedSuits = new Set(['Spades', 'Clubs', 'Diamonds', 'Hearts']);
+    const collectSuitsInDeck = _propSetInDeck(deck.cards, 'suit', 4);
 
-    function collectsuitsInDeck(deck) {
-      const suits = new Set();
-      let i = 0;
-      const cards = deck.cards;
-      while (suits.size < 4) {
-        const suit = cards[i].suit;
-        if (!suits.has(suit)) {
-          suits.add(suit);
-        }
-        i++;
+    expect(expectedSuits).toEqual(collectSuitsInDeck);
+  });
+
+  it('should have the 13 ranks', () => {
+    const expectedRanks = new Set([
+      'Ace',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      'Jack',
+      'King',
+      'Queen'
+    ]);
+    const collectRanksInDeck = _propSetInDeck(deck.cards, 'rank', 13);
+
+    expect(expectedRanks).toEqual(collectRanksInDeck);
+  });
+
+  it('should have a range of values 1..13', () => {
+    const expectedRange = [1, 13];
+    function getRange(cards) {
+      let low = Infinity;
+      let high = -Infinity;
+      for (const card of cards) {
+        if (card.value < low) low = card.value;
+        if (card.value > high) high = card.value;
       }
-      return suits;
+      return [low, high];
     }
 
-    expect(expectedSuits).toEqual(collectsuitsInDeck(deck));
+    expect(expectedRange).toEqual(getRange(deck.cards));
   });
 });
--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,7 @@
-class Card {
+module.exports = class Card {
   constructor(suit, rank, value) {
     this.suit = suit;
     this.rank = rank;
     this.value = value;
   }
-}
-
-module.exports = Card;
+};
